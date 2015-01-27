afb79ac2c8e7cd8abaef894ae1ea9837361e4e87
redtankd <redtankd@qq.com>
2015-01-27 17:41:25 +0800

    the solution for project euler's no.4 problem

---
+++ src/00004_A.rs
@@ -0,0 +1,18 @@
+
+fn main() {
+    let mut max = (0, 0, 0);
+
+    for i in (100..1000).rev() {
+        for j in (100..i).rev() {
+            let p = i*j;
+            if p > max.0 { continue; }
+            let str = p.to_string();
+            let strs = str.as_slice();
+            if strs.chars().rev().collect::<String>() == str {
+                max = (p, i, j);
+            }
+        }
+    }
+
+    println!("{}, {}, {}", max.0, max.1, max.2);
+}
---
+++ src/00004_B.rs
@@ -0,0 +1,25 @@
+
+fn main() {
+    let mut it: Vec<(u32, u32)> = Vec::new();
+    for i in (100..1000).rev() {
+        for j in (100..i).rev() {
+            it.push((i,j));
+        }
+    }
+
+    let mut max = 0u32;
+
+    while let Some(p) = it.iter()
+        .map(|&(x, y)| x*y)
+        .filter(|&x| { // great for performance
+            x > max
+        })
+        .filter(|&x| {
+            let str = x.to_string();
+            str.chars().rev().collect::<String>() == str
+        }).next() {
+        max = p;
+    }
+
+    println!("{}", max);
+}
