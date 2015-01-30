6eced871b1162e46e3318739876a9e5109c3e9e5
redtankd <redtankd@qq.com>
2015-01-30 17:18:25 +0800

    new solutions for project euler

---
+++ src/00012.rs
@@ -0,0 +1,19 @@
+/*
+so slow
+
+i=12375,t=76576500,n=576
+
+real    3h7m11.82s
+user    2h21m19.63s
+sys     0m6.64s
+*/
+fn main() {
+    for i in (1u32..) {
+        let t = (1..i+1).fold(0, |sum, x| sum + x);
+        let n = (1..t+1).filter(|&x| t % x == 0).count();
+        if n > 500 {
+            println!("i={},t={},n={}", i, t, n);
+            break;
+        }
+    }
+}
