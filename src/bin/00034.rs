8ed049c4cb2899b7b2835f8426968f7a6295843c
redtankd <redtankd@qq.com>
2018-03-14 21:01:32 +0800

    problem 34

---
+++ src/bin/00034.rs
@@ -0,0 +1,36 @@
+#![feature(test)]
+
+extern crate test;
+extern crate project_euler;
+
+use project_euler::*;
+
+fn main() {
+    let t = start_timer();
+
+    println!("\nsolution:");
+    println!("The answer is {:?}\n", s1());
+
+    stop_timer(t);
+}
+
+fn s1() -> u32 {
+    (11u32..).filter(|n| 
+        *n == n.to_string()
+            .chars()
+            .map(|c| u32::from(c)-48)
+            .map(|c| (1..c+1).fold(1, |pn, p| pn*p))
+            .sum::<u32>()
+    ).take(2)
+    .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::s1;
+
+    #[test]
+    fn test_s1() {
+        assert_eq!(40730, s1());
+    }
+}
