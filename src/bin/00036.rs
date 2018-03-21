f2ac1c8d2a13f46c1c7fd4131dd66709faca40c0
redtankd <redtankd@qq.com>
2018-03-21 21:40:45 +0800

    problem 36

---
+++ src/bin/00036.rs
@@ -0,0 +1,36 @@
+#![feature(test)]
+
+extern crate test;
+extern crate project_euler;
+
+use project_euler::*;
+
+fn main() {
+    let t = start_timer();
+
+    println!("\nsolution:");
+    println!("The answer is {:?}\n", s1());
+
+    stop_timer(t);
+}
+
+fn s1() -> u32 {
+    (1..1000000)
+        .filter(|x| {
+            let binary = format!("{:b}", x);
+            let decimal = format!("{}", x);
+            decimal.chars().rev().collect::<String>() == decimal
+            && binary.chars().rev().collect::<String>() == binary
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::s1;
+
+    #[test]
+    fn test_s1() {
+        assert_eq!(872187, s1());
+    }
+}
